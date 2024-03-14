056a7dc2b04a020c7bad569088761e71afd07b46
TarunChopra[MSFT] <tarunchopra@live.com>
2024-03-14 11:04:22 -0700

    Update drise.py

--- python/vision_explanation_methods/explanations/drise.py
+++ python/vision_explanation_methods/explanations/drise.py
@@ -383,20 +383,20 @@ def DRISE_saliency_for_mlflow(
         update_sharepoint_list_item()       
     return saliency_fusion(mask_records, device, verbose=verbose)
 
-def update_sharepoint_list_item():
-      endpoint = sharepoint_config['url']
-      headers = {
-          "Accept": "application/json;odata=verbose",
-          "Content-Type": "application/json;odata=verbose",
-          "X-HTTP-Method": "MERGE",
-          "If-Match": "*"
-      }
-      auth = HTTPBasicAuth(sharepoint_config['username'], sharepoint_config['password'])
-      
-      try:
-          response = requests.post(endpoint, json=sharepoint_config['data'], headers=headers, auth=auth)
-          response.raise_for_status()
-          print("Item updated successfully.")
-      except requests.exceptions.RequestException as e:
-          print("Error updating item:", e)
 
+def update_sharepoint_list_item():
+    endpoint = sharepoint_config['url']
+    headers = {
+        "Accept": "application/json;odata=verbose",
+        "Content-Type": "application/json;odata=verbose",
+        "X-HTTP-Method": "MERGE",
+        "If-Match": "*"
+    }
+    auth = HTTPBasicAuth(sharepoint_config['username'], sharepoint_config['password'])
+      
+    try:
+        response = requests.post(endpoint, json=sharepoint_config['data'], headers=headers, auth=auth)
+        response.raise_for_status()
+        print("Item updated successfully.")
+    except requests.exceptions.RequestException as e:
+        print("Error updating item:", e)
